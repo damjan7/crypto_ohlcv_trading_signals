01916c5379fb7e657b5fe93c8277948a9ad888ce
Damjan Kostovic <damjan.kostovic@hotmail.com>
2025-02-18 18:07:29 +0100

    implement data_processor with a dataclass logic (creates feature and targets upon init)

--- dev/binance/data_processor.py
+++ dev/binance/data_processor.py
@@ -1,10 +1,11 @@
-import signal_generator as sg
+import feature_generator as fg
 import pandas as pd
 import polars as pl
 import os
 import numpy as np
 import plotly.express as px
-from typing import List
+from typing import List, Dict, Tuple
+
 
 def create_feature_dataset(df: pd.DataFrame) -> pd.DataFrame:
     '''
@@ -14,23 +15,28 @@ def create_feature_dataset(df: pd.DataFrame) -> pd.DataFrame:
     Also returns a list with all the names of the features
     '''
 
+    df = df.copy()
+
     # Initialize
-    config = sg.SignalConfig()
-    generator = sg.SignalGenerator(config)
+    config = fg.Config()
+    generator = fg.FeatureGenerator(config)
     
     # Add all features
-    generator.add_feature(sg.ReturnFeatures())
-    generator.add_feature(sg.VolatilityFeatures())
-    generator.add_feature(sg.VolumeFeatures())
-    generator.add_feature(sg.PriceMAFeatures())
-    generator.add_feature(sg.RSIFeature(config))
-    generator.add_feature(sg.EMAFeature(config))
-    generator.add_feature(sg.BollingerBandsFeature(config))
+    # manual so far, but no big deal
+    generator.add_feature(fg.ReturnFeatures())
+    generator.add_feature(fg.VolatilityFeatures())
+    generator.add_feature(fg.VolumeFeatures())
+    generator.add_feature(fg.PriceMAFeatures())
+    generator.add_feature(fg.RSIFeature(config))
+    generator.add_feature(fg.EMAFeature(config))
+    generator.add_feature(fg.BollingerBandsFeature(config))
 
     for feature in generator.features:
         feature_df = feature.calculate(df)
     
     feature_names = generator.get_all_feature_names()
+    feature_df.index = feature_df.Date
+    feature_df.drop(["Date"], axis=1, inplace=True)
     return feature_df, feature_names
 
 def create_target_dataset(
@@ -58,27 +64,57 @@ def create_target_dataset(
             raise ValueError("target_type must be either 'classification' or 'regression'")
     
     target_names = [f'target_regr_horizon{horizon}' for horizon in horizon_lst]
-    target_df = df.loc[:, target_names]
-    target_df['Date'] = df.loc[:, ["Date"]]
+    target_df = df.loc[:, target_names + ['Date']]
+    target_df.index = df.Date
+    #target_df.drop(["Date"], axis=1, inplace=True)
     return target_df, target_names
 
+def create_target_datasets_for_pairs(
+    pairs: List[str],
+    input_path: str
+) -> Tuple[Dict[str, pd.DataFrame], List[str]]:
+    out_dict = {}
+    for pair in pairs:
+        df = pl.read_parquet(f'{input_path}/{pair.replace("/", "")}.parquet').to_pandas()
+        out_dict[pair], target_names = create_target_dataset(df)
+    return out_dict, target_names
+
+def create_feature_ds_dict_for_pairs(
+    pairs: List[str], 
+    input_path: str
+) -> Tuple[Dict[str, pd.DataFrame], List[str]]:
+    out_dict = {}
+    for pair in pairs:
+        df = pl.read_parquet(f'{input_path}/{pair.replace("/", "")}.parquet').to_pandas()
+        out_dict[pair], feat_names = create_feature_dataset(df)
+    return out_dict, feat_names
 
 
+def create_cross_sectional_ds(dict: Dict[str, pd.DataFrame]) -> pd.DataFrame:
+    """
+    Given a feature Dictionary, creates a cross sectional dataset
+    """
 
 #def calc_feature_dataset_for_pairs() 
     
 
-#### THIS SHOULD BE CALLED IN MAIN FILE ####
-TICKER_DATA_PATH = r"C:\Users\Damja\CODING_LOCAL\trading\data\ticker_specific_data_BINANCE"
-pairs = pd.read_csv(r"dev\binance\pairs.csv")
-NUM_PAIRS_TO_LOAD = 100
-pairs = pairs.iloc[:NUM_PAIRS_TO_LOAD, 0].values
-pairs = [pair.replace("USD", "USDT") for pair in pairs]
-pair = pairs[2]
-df = pl.read_parquet(f'{TICKER_DATA_PATH}/{pair.replace("/", "")}.parquet').to_pandas()
+class DataClass():
+    """
+    Data Class that holds all the data that is then used in the signal generator
 
-df, feature_names = create_feature_dataset(df)
+    TODO:
+    - implement the above funcs in the class structure? or not?
+    """
+    def __init__(self, pairs: List[str], input_path: str):
+        self.feature_dict, self.feature_names = create_feature_ds_dict_for_pairs(pairs=pairs, input_path=input_path)
+        self.target_dict, self.target_names = create_target_datasets_for_pairs(pairs=pairs, input_path=input_path)
 
-target_df, target_names = create_target_dataset(df, target_type='regression')
+    def get_feature_names(self):
+        return self.feature_names
 
-print("done")
+    def summarize_data(self):
+        pass
+
+    def normalize_features(self, feature_names: List[str]):
+        pass
+
